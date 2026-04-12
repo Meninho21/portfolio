b15113bcbc0bc9d00681645e5fc8fd5f052f50b2
Filomeno ANTUNES <filomeno_antunes@hotmail.com>
2026-04-12 14:34:51 +0200

    Change game title and scene in Minigame.js

---
+++ Minigame.js
@@ -0,0 +1,21 @@
+import { AIRevolution } from './scenes/AIRevolution.js';
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Developer vs AI',
+    description: '',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    pixelArt: false,
+    scene: [
+        AIRevolution
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+}
+
+new Phaser.Game(config);
